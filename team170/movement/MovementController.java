43d2f5d3e4922bcbd19faf09962a6127e9dbbf7b
Aaron Wojnowski <aaronwojnowski@gmail.com>
2015-01-08 15:14:47 -0600

    Add movement.

---
+++ team170/movement/MovementController.java
@@ -0,0 +1,91 @@
+package team170.movement;
+
+import battlecode.common.*;
+import team170.*;
+
+public class MovementController {
+	
+	public Robot robot;
+	
+	private int stopTurns;
+	
+	// MARK: Directions
+	
+	public int directionToInt(Direction d) {
+		switch(d) {
+			case NORTH:
+				return 0;
+			case NORTH_EAST:
+				return 1;
+			case EAST:
+				return 2;
+			case SOUTH_EAST:
+				return 3;
+			case SOUTH:
+				return 4;
+			case SOUTH_WEST:
+				return 5;
+			case WEST:
+				return 6;
+			case NORTH_WEST:
+				return 7;
+			default:
+				return -1;
+		}
+	}
+
+	// MARK: Movement
+
+	public Boolean shouldMove() {
+		
+		return this.stopTurns == 0;
+		
+	}
+	
+	public void moveToward(MapLocation location) throws GameActionException {
+		
+		if (location == null) return; 
+		if (!this.shouldMove()) return;
+		
+		Direction direction = this.robot.robotController.getLocation().directionTo(location);
+		int directionInteger = this.directionToInt(direction);
+		
+		int[] offsets = {0,1,-1,2,-2};
+		for (int offset : offsets) {
+			
+			direction = this.robot.DIRECTIONS[(directionInteger + offset + 8) % 8];
+			if (this.moveTo(direction)) return;
+			
+		}
+		
+	}
+	
+	public Boolean moveTo(Direction direction) throws GameActionException {
+		
+		if (!this.shouldMove()) return false;
+		
+		if (this.robot.robotController.canMove(direction)) {
+			
+			this.robot.robotController.move(direction);
+			return true;
+			
+		}
+		return false;
+		
+	}
+	
+	public void stopFor(int turns) {
+		
+		this.stopTurns = turns;
+		
+	}
+	
+	// MARK: Move Turns
+	
+	public void decrementMoveTurns() {
+	
+		this.stopTurns = Math.max(0, this.stopTurns - 1);
+	
+	}
+
+}
