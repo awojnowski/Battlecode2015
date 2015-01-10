a48279d2d8f3d7ddba61c3adb93fb3e0b2f65b13
Aaron Wojnowski <aaronwojnowski@gmail.com>
2015-01-10 11:52:40 -0600

    Add updated save team python file.

--- save_team.py
+++ save_team.py
@@ -4,7 +4,7 @@ import time
 name = "team170";
 
 timestamp = str(int(time.time()))
-new_name = name + "v" + timestamp;
+new_name = raw_input("Enter the new name for this saved bot: ");
 
 os.system("cp -R " + name + " " + new_name);
 os.system("find " + new_name + "/* -name \"*.java\" -print0 | xargs -0 sed -i '' -e 's/" + name + "/" + new_name + "/g'");
